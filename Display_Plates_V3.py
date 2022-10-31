aa8df7e1ee0dcf54ffe7c3d5fc8ae839e0c0d9b2
Wayne400 <thevalman@hotmail.com>
2022-10-31 22:05:34 +1100

    Menu for pictures

--- Display_Plates_V3.py
+++ Display_Plates_V3.py
@@ -442,9 +442,9 @@ class Picture(object):
 
     def __init__(self, picture_index="", rego_plate="", jurisdiction="", rego_sticker="",
                  iso_publication_date="", page_number=0, magazine="",
-                 make="", model="",
-                 description="",
-                 car_year="none",model_code =""):
+                 make="", model="", description="",
+                 car_year="none", model_code ="",
+                 model_level="none",trim_level=""):
 
         self.picture_index = picture_index
         self.title = rego_plate
@@ -458,6 +458,8 @@ class Picture(object):
         self.description = description
         self.year = car_year
         self.model_code = model_code
+        self.model_level = model_level
+        self.trim_level = trim_level
 
 
     def print_picture(self):
@@ -471,11 +473,17 @@ class Picture(object):
             self.rego_sticker = ""
         if self.model_code == "none":
             self.model_code = ""
+        if self.model_level == "none":
+            self.model_level = ""
+        if self.trim_level == "none":
+            self.trim_level = ""
+        combined_description = ""
+        combined_description = self.make + " " + self.model_code + " " + self.model + " " + self.model_level + " " + self.trim_level
+#        combined_description = self.make + " " + self.model_code + " " + self.model
 
-
-        print('{0:6} {1:3} {2:5} {3:4} {4:16} {5:2} {6:16} {7:16} {8:24} p{9:3} {10:11} '.format(
-            self.title, self.jurisdiction, self.rego_sticker, self.year, self.make, self.model_code,
-            self.model, self.description, self.magazine, str(self.page_number), self.iso_publication_date))
+        print('{0:6} {1:3} {2:5} {3:4} {4:30} {5:40} {6:24} p{7:3} {8:11} '.format(
+            self.title, self.jurisdiction, self.rego_sticker, self.year, combined_description,
+            self.description, self.magazine, str(self.page_number), self.iso_publication_date))
 
 
 
@@ -724,7 +732,7 @@ def main():
                 else:
                     Rambler_list = ["all"]
 
-                ads_table = get_sql_data(car_model_list=Rambler_list, car_make="Rambler",
+                ads_table = get_sql_data(car_model_list=Rambler_list, car_make="Rambler",car_model=pick_model,
                                          connectstring="../advertisements_indexed.db",
                                          jurisdiction=pick_state, publication_year=pick_year,
                                          publication=pick_publication)
@@ -1042,7 +1050,8 @@ def main():
                           iso_publication_date=pics_record[4], page_number=pics_record[5],
                           magazine=pics_record[6], make=pics_record[7],
                           model=pics_record[8], description=pics_record[9],
-                          car_year=pics_record[10], model_code=pics_record[11])
+                          car_year=pics_record[10], model_code=pics_record[11],
+                          model_level=pics_record[12], trim_level=pics_record[13])
         pics_dict[pics_record[0]] = new_pic
     for pic in pics_dict:
         new_pic = pics_dict[pic]
--- Load_Wheels_Menu.py
+++ Load_Wheels_Menu.py
@@ -40,7 +40,7 @@ def add_pictures( row):
         master_index1 = 100000
 #        picture_index = 1
         sql = 'SELECT MAX(picture_index) FROM pictures'
-#        print(sql)
+        print(sql)
         try:
             results = cursor.execute(sql)
             max_index = results.fetchall()
@@ -74,135 +74,259 @@ def add_pictures( row):
         conn.commit()
         conn.close()
 
+def Do_Car_Sales_Menu(car_make):
 
+    Data_Fill = { "car_make": car_make,
+                  "car_model": 'none',
+                  "model_code": 'none',
+                  "model_level": 'none',
+                  "trim_level": 'none',
+                  "model_year": 'none',
+                  }                                     # 25 - 28
+
+    dont_ask = {
+        'Alfa': ['car_make', 'model_code', 'model_level', 'trim_level'],
+        'Chrysler': [ 'car_make'],
+        'Citroen': [ 'car_make', 'model_code', 'model_level'],
+        'Datsun': [ 'car_make', 'model_code', 'model_level', 'trim_level'],
+        'Ford': [ 'car_make', 'model_level'],
+        'Fiat': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Holden': [ 'car_make', "model_level"],
+        'Honda': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Leyland': [ 'car_make', 'model_code'],
+        'Mazda': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Mercedes': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Peugeot': [ 'car_make', "model_code", "model_level"],
+        'Rambler': [ 'car_make', 'model_code', 'model_level'],
+        'Renault': [ 'car_make', "model_code", "model_level"],
+        'Rolls Royce': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Rover': [ 'car_make', "model_level"],
+        'Toyota': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Triumph': [ 'car_make', "model_level"],
+        'Volkswagen': [ 'car_make', 'trim_level', 'model_level']
+    }
+
+    must_ask = {
+        'Alfa': ['car_model', 'trim_level'],
+        'Chrysler': ['car_model', 'model_code', 'model_level', 'trim_level'],
+        'Citroen': [ 'car_make', 'model_code', 'model_level'],
+        'Datsun': [ 'car_make', 'model_code', 'model_level', 'trim_level'],
+        'Ford': ['car_model', 'model_code', 'trim_level'],
+        'Fiat': [ 'car_model'],
+        'Holden': [ 'car_model','model_code', 'trim_level'],
+        'Honda': [ 'car_model', "model_code", "model_level", "trim_level"],
+        'Leyland': [ 'car_model', 'model_level', 'trim_level'],
+        'Mazda': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Mercedes': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Peugeot': [ 'car_make', "model_code", "model_level"],
+        'Rambler': [ 'car_model', 'model_code', 'model_level'],
+        'Renault': [ 'car_model', "model_code", "model_level"],
+        'Rolls Royce': [ 'car_make', "model_code", "model_level", "trim_level"],
+        'Rover': [ 'car_model', "model_level"],
+        'Subaru': ['car_model'],
+        'Toyota': [ 'car_model', "model_code", "trim_level"],
+        'Triumph': ['car_model', "trim_level", "model_code"],
+        'Volkswagen': [ 'car_model', 'model_code'],
+        'Volvo': [ 'car_model', 'trim_level']
+    }
+
+
+    Make_Dict = {}
+    Model_Code = {}
+    Model_Level = {}
+    Trim_Level = {}
+    clues = {}
+    clues['model_year'] = '1969'
+
+    Make_Dict["Alfa"] = ['Alfasud','Alfetta']
+    Trim_Level["Alfa", "Alfasud"] = [ 'Ti']
+    Trim_Level["Alfa", "Alfetta"] = ['none']
+
+    Make_Dict['Chrysler'] = ['Valiant', 'Dodge', 'Centura']
+    Model_Code['Chrysler', 'Dodge'] = ['Phoenix', 'Custom 880']
+    Model_Code['Chrysler', 'Centura'] = ['KB', 'KC']
+    Model_Code['Chrysler', 'Valiant'] = ["R", "S", "AP5", "AP6", "VC", "VE", "VF", "VG", "VH", "VJ", "VK", "CL", "CM"],
+    Model_Level['Chrysler', 'Valiant'] = ["Valiant", "Regal", "Safari", "VIP", "Pacer", "Ranger", "Charger"],
+    Model_Level['Chrysler', 'Centura'] = ["none"],
+    Trim_Level['Chrysler', 'Valiant'] = ["XL", "770", "Drifter", "E31", "E34", "E38", "E48", "E49"],
+    Trim_Level['Chrysler', 'Centura'] = ["XL", "GL", "GLX"],
+
+    Make_Dict["Citroen"] = ['GS']
+    Trim_Level["Citroen", "DS"] = ['Pallas']
+    Trim_Level["Citroen", "GS"] = [ 'Club']
+
+    Make_Dict["Datsun"] = ['180B', '120Y', '1600']
+
+    Make_Dict["Fiat"] = ['124S', '132', '127']
+
+    Make_Dict["Ford"] = ["Falcon", "Cortina", "Capri", "Escort"]
+    Trim_Level['Ford', "Falcon"] = ['XL', 'GS', 'GTHO', 'GT']
+    Trim_Level['Ford', "Cortina"] = ['Lotus', 'XL', 'XLE']
+    Trim_Level['Ford', "Escort"] = ['RS2000', 'GHIA','XL','Little Ripper']
+    Model_Code['Ford', "Falcon"] = ['XL', 'XM', 'XT', 'XR', 'XT', 'XW', 'XY', 'XA', 'XB', 'XC']
+    Model_Code['Ford', "Cortina"] = ['MKI', 'MKII', 'TC', 'TD', 'TE', 'TF']
+    Model_Code['Ford', "Escort"] = ['MKI', 'MKII']
+
+    Make_Dict['Holden'] = ["Standard", "Special", "Kingswood", "Torana", "Premier", "Monaro", "Belmont", "Statesman", "Gemini"]
+    Trim_Level['Holden', 'Kingswood'] = ['SL', 'SS']
+    Trim_Level['Holden', 'Statesman'] = ['De Ville', 'Caprice', 'none']
+    Trim_Level['Holden', 'Monaro'] = ['GTS', 'GTS 327', 'GTS 350']
+    Model_Code["Standard"] = ['EJ', 'EH', 'HD', 'HR']
+    Model_Code["Special"] = ['EJ', 'EH', 'HD', 'HR']
+    Model_Code["Premier"] = ['EJ', 'EH', 'HD', 'HR']
+    Model_Code["Brougham"] = ['HK', 'HT', 'HG']
+    Model_Code['Holden', 'Kingswood'] = ['HK', 'HT', 'HG', 'HQ', 'HJ', 'HZ']
+    Model_Code['Holden', 'Monaro'] = ['HK', 'HT', 'HG', 'HQ', 'HJ', 'HZ']
+    Model_Code['Holden', 'Belmont'] = ['HK', 'HT', 'HG', 'HQ', 'HJ', 'HZ']
+    Model_Code['Holden', 'Statesman'] = ['HQ', 'HJ', 'HZ', 'WB']
+    Model_Code['Holden', 'Torana'] = ['HB', 'LC', 'LH', 'LJ', 'LX', 'UC']
+    Trim_Level['Holden', 'Torana'] = ['S', 'SL', 'XU-1', 'SLE', 'SLR/5000']
+    Model_Code['Holden', 'Gemini'] = ['TX', 'TC', 'TD', 'TF', 'TG']
+    Trim_Level['Holden', 'Gemini'] = ['S', 'SL', 'Coupe']
+
+    Make_Dict["Honda"] = ['s600', 'Civic', 'Z', '1500']
+
+    Make_Dict['Leyland'] = ["P76", "Austin", "Morris", "Mini", "Wolseley"]
+    Model_Level['Leyland', "Austin"] = ["Freeway", "1800", "Tasman", "Kimberly"]
+    Model_Level['Leyland', "Morris"] = ["Marina", "1100", "1300", "1500", "Nomad"]
+    Model_Level['Leyland', "P76"] = ["none"]
+    Model_Level['Leyland', "Mini"] = ['850', '1100']
+    Trim_Level['Leyland', "Austin"] = ["none"]
+    Trim_Level['Leyland', "P76"] = ['Super', 'Executive', 'Super Six', 'De Luxe', 'Targa Florio']
+    Trim_Level['Leyland', "Morris"] = ['Super', 'Executive', 'Super Six', 'De Luxe']
+    Trim_Level['Leyland', "Mini"] = [ 'Deluxe']
+
+    Make_Dict["Mazda"] = ["RX4", "Capella", "1300", "1800"]
+
+    Make_Dict["Mercedes"] = ["280"]
+
+    Make_Dict["Peugeot"] = ["403", "403B", "404", "504", "203", "203C"]
+    Trim_Level["Peugeot"] = ['none']
+
+    Make_Dict["Rambler"] = ["Gremlin", "Hornet", "Matador", "Rebel", "Classic", "Ambassador", "Javelin", "American",
+                            "AMX", "Marlin", "X-Coupe", "Wagon"]
+    Trim_Level["Hornet"] = ['SST']
+    Trim_Level["Javelin"] = ['SST']
+    Trim_Level["Ambassador"] = ['SST']
+    Trim_Level["Classic"] = ['330', '440', '660', '770']
+    Trim_Level["American"] = ['330', '440', '660', '770']
+
+    Make_Dict["Renault"] = ["R4", "R8", "R10", "R12", "R16", "R10S", "10S", "R15", "R17", "1.4", "RXX"]
+    Trim_Level["Renault"] = ['GL', 'TL', 'TS', 'Gordini']
+
+    Make_Dict["Rolls Royce"] = ["Silver Shadow"]
+
+    Make_Dict["Rover"] = ["105R", "105S", "2000", "2000TC", "3500", "P5B", "P5", "P5Bcoupe", "P5coupe", "3L", "100"]
+    Trim_Level['Rover', 'P5B'] = [ "Coupe", "Sedan"],
+    Trim_Level['Rover', '2000'] = ["TC", "S"],
+    Model_Code["Rover"] = ['MKI', 'MKII', 'MKIII']
+    Model_Code['Rover', '2000'] = ['MKI', 'MKII', 'MKIII']
+
+    Make_Dict["Subaru"] = ["Sedan", "Coupe", "Wagon", "4WD Wagon"]
+
+    Make_Dict["Toyota"] = ['Crown', '1900', 'Corolla', 'Corona', 'Celica']
+    Trim_Level["Toyota",'Corona'] = ['SE']
+    Trim_Level["Toyota",'Corolla'] = ['SE']
+    Trim_Level["Toyota",'Crown'] = ['Super Saloon']
+    Model_Code['Toyota', 'Corona'] = ['MKI', 'MKII', 'none']
+    Model_Code['Toyota', 'Crown'] = ['none']
+
+
+    Make_Dict['Triumph'] = ['2000', '2500', '2.5', 'Stag']
+    Trim_Level['Triumph', '2.5'] = ["TC", "PI", "S"],
+    Trim_Level['Triumph', '2000'] = ["TC"],
+    Trim_Level['Triumph', '2500'] = ["TC", "S"],
+    Trim_Level['Triumph', 'Stag'] = ['none'],
+    Model_Code['Triumph', '2.5'] = ['MKI', 'MKII']
+    Model_Code['Triumph', '2000'] = ['MKI', 'MKII']
+    Model_Code['Triumph', '2500'] = ['MKI', 'MKII']
+    Model_Code['Triumph', 'Stag'] = ['none']
+
+    Make_Dict["Volkswagen"] = ["Beetle", "Bug", "Super Bug", "1200", "1500", "1600", "Karman Ghia", "Kombi Van",
+                               "Passat", "Camper Van"]
+    Model_Code['Volkswagen', '1200'] = ['Type 1']
+    Make_Dict["Volvo"] = ["142", "144", "145", "164", "242", "244" , "245", "264"]
+    Trim_Level['Volvo', '244'] = ['DL', 'GL']
+    Trim_Level['Volvo', '264'] = ['GL']
+
+
+    # for question in must_ask:
+    clues["car_model"] = Make_Dict[car_make]
+    retry = False
+    for question in must_ask[car_make]:
+            if question == 'model_code':
+                # print('model_code', Data_Fill['car_model'])  # stored previously
+                clues['model_code'] = Model_Code[car_make, Data_Fill['car_model']]
+            if question == 'model_level':
+                # print('model_level', Data_Fill['car_model'])
+                clues['model_level'] = Model_Level[car_make, Data_Fill['car_model']]
+            if question == 'trim_level':
+                # print('trim_level', Data_Fill['car_model'] )
+                clues['trim_level'] = Trim_Level[car_make, Data_Fill['car_model']]
+            print(clues[question])
+            answer = input(question + "<" + Data_Fill[question] + ">")
+            if answer == "":
+                answer = Data_Fill[question]
+            if question == "car_model":
+                if answer not in Make_Dict[car_make]:
+                    print("wally")
+                    Data_Fill[question] = 'none'
+                else:
+                    Data_Fill[question] = answer
+            else:
+                Data_Fill[question] = answer
+    return Data_Fill
+
+def Menu(Default_Data_Fill):
+
+
+    Make_list = ["Ford", "Leyland", "Peugeot", "Rambler", "Renault", "Rover", "Triumph", "Valiant", "Volkswagen",
+                 "x = exit"]
+    print(Make_list)
+    car_make = input("Car Make?")
+    Data_Fill  = Do_Car_Sales_Menu(car_make)
+    clues = {
+        "rego_plate": 'RAM232',
+        "jurisdiction": 'NSW',
+        "rego_sticker": 'none',
+        'magazine': "Wheels",
+        "iso_publication_date": '1972-07-01',
+        'page_number': "28",
+        "model_year": 'none',
+        "description": 'none'
+    }
+
+    # for question in must_ask:
+    retry = False
+    for question in clues:
+            print(clues[question])
+            answer = input(question + "<" + clues[question] + ">")
+            if answer == "":
+                Data_Fill[question] = clues[question]
+            else:
+                Data_Fill[question] = answer
+
+    return Data_Fill
 
 def main():
+    Default_Fill = {
+                  "master_index": '99999999',
+    }
+
     pics_table = get_sql_pictures_all(connectstring="../advertisements_indexed.db", jurisdiction="all",
                                  publication="all", publication_year="all")
     for pics_record in pics_table:
         print(pics_record[0], pics_record[1], pics_record[2], pics_record[3], pics_record[4], pics_record[5],
               pics_record[6], pics_record[7], pics_record[8], pics_record[9], pics_record[10], pics_record[11])
 
-    Data_Fill = { "car_make": 'aston martin',
-                  "master_index": '99999999',
-                  "jurisdiction": 'NSW',
-                  "rego_plate": 'ABC123',
-                  "rego_sticker": '5-69',
-                  "magazine": 'Wheels',
-                  "iso_publication_date": '1973-09-01',
-                  "page_number": 1,
-                  "car_model": 'DB5',
-                  "model_code": 'VG',
-                  "model_level": 'none',
-                  "trim_level": 'none',
-                  "model_year": '1949',
-                  "description": 'E49 six pack limelight'}                                     # 25 - 28
 
     while True:
-
-#        dont_ask = {
-#            'master_index': '99999999',
-#        }
-        dont_ask = {
-            'Leyland': ['master_index', 'car_make', 'model_code'],
-            'Rambler': ['master_index', 'car_make', 'model_code', 'model_level' ],
-            'Ford': ['master_index', 'car_make'],
-            'Renault': ['master_index', 'car_make', "model_code", "model_level"],
-            'Peugeot': ['master_index', 'car_make', "model_code", "model_level"],
-            'Rover': ['master_index', 'car_make', "model_level"],
-            'Triumph': ['master_index', 'car_make', "model_level"]
-        }
-        must_ask = {
-            "rego_plate": 'RAM232',
-            "jurisdiction": 'NSW',
-            "rego_sticker": 'none',
-            'magazine': "Modern Motor",
-            "iso_publication_date": '1971-07-01',
-            'page_number': "none",
-            'model_year': '1970',
-            'car_make': 'No Default',
-            "model_code": "none",
-            "car_model": "none",
-            "model_level": "none",
-            "trim_level": "none",
-            "description": 'none'
-        }
-
-        clues = {
-            "rego_plate": 'RAM232',
-            "jurisdiction": 'NSW',
-            "rego_sticker": '2-72',
-            'magazine': "Wheels",
-            "iso_publication_date": '1972-07-01',
-            'page_number': "28",
-            'model_year': '1966',
-            'car_make': 'Ford',
-            "model_code": "XY",
-            "car_model": ["Falcon"],
-            "model_level": ['none'],
-            "trim_level": ['none'],
-            "description": 'GTHO Phase III'
-        }
-        Make_Dict = {}
-        Model_Dict = {}
-        Model_Code = {}
-        Trim_Level = {}
-        Make_Dict["Rambler"] = ["Gremlin", "Hornet", "Matador", "Rebel", "Classic", "Ambassador", "Javelin", "American",
-                        "AMX", "Marlin", "X-Coupe", "Wagon"]
-        Trim_Level["Rambler"] = ['330', '440', '660', '770', 'SST']
-
-        Make_Dict["Ford"] = ["Falcon", "Cortina", "Capri"]
-        Trim_Level["Ford"] = ['XL', 'XLE', 'GHIA', 'GS', 'GTHO', 'GT', 'Lotus']
-        Model_Code["Ford"] = ['XL', 'XM', 'XT', 'XR', 'XT', 'XW', 'XY','XA','XB','XC']
-
-        Make_Dict["Renault"] = ["R4", "R8", "R10", "R12", "R16", "R10S", "10S", "R15", "R17", "1.4", "RXX"]
-        Trim_Level["Renault"] = ['GL', 'TL', 'TS', 'Gordini']
-
-        Make_Dict["Peugeot"] = ["403", "403B", "404", "504", "203", "203C"]
-        Trim_Level["Peugeot"] = ['none']
-
-        Make_Dict["Rover"] = ["105R", "105S", "2000", "2000TC", "3500", "P5B", "P5", "P5Bcoupe", "P5coupe", "3L", "100"]
-        Trim_Level['Rover'] = ["TC", "Coupe", "S"],
-        Model_Code["Rover"] = ['MKI', 'MKII', 'MKIII']
-
-        Make_Dict["Triumph"] = ["2000", "2500"]
-        Trim_Level['Triumph'] = ["TC", "PI", "S"],
-        Model_Code["Triumph"] = ['MKI', 'MKII']
-
-        Make_Dict["Leyland"] = ["P76", "Austin", "Morris", "Mini", "Wolseley"]
-        Model_Dict["Austin"] = ["Freeway", "1800", "Tasman", "Kimberly"]
-        Model_Dict["Morris"] = ["Marina", "1100", "1300", "1500", "Nomad"]
-        Model_Dict["P76"] = ["none"]
-        Trim_Level["Leyland"] = ['Super', 'Executive', 'Super Six', 'De Luxe']
-
-        Make_Dict["Volkswagen"] = ["Beetle", "Bug", "Super Bug", "1500", "1600", "Karman Ghia", "Kombi Van", "Camper Van"]
-        make_found = False
-        car_make = "Wallmeister"
-        while make_found == False:
-            car_make = input("Car Make?")
-            if car_make in dont_ask:
-                make_found = True
-
-        # for question in must_ask:
-        clues["car_model"] = Make_Dict[car_make]
-        clues["trim_level"] = Trim_Level[car_make]
-        for question in Data_Fill:
-            if question not in dont_ask[car_make]:
-                if question == "model_code":
-                    clues["model_code"] = Model_Code[car_make]
-                print(clues[question])
-                answer = input(question + "<" + must_ask[question] + ">")
-                if answer == "":
-                    answer = must_ask[question]
-                if question == "car_model" and answer in Model_Dict:
-                    clues["model_level"] = Model_Dict[answer]
-
-
+        Data_Fill = Menu(Default_Fill)
         add_pictures( Data_Fill)
         pics_table = get_sql_pictures_all(connectstring="../advertisements_indexed.db", jurisdiction="all",
                                  publication="all", publication_year="all")
         for pics_record in pics_table:
-            print(pics_record[0], pics_record[1], pics_record[2], pics_record[3], pics_record[4], pics_record[5],
+            print(pics_record[0], pics_record[1], pics_record[2], pics_record[3], pics_record[4], "page", pics_record[5],
                           pics_record[6], pics_record[7], pics_record[8], pics_record[9], pics_record[10],
                           pics_record[11])
         plate = Data_Fill["rego_plate"][0:3]
